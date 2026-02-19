caf28461f42620e1b396ba36f80a9dbb6ccf6fc7
Ravi Pangali <lilathapamagar453@gmail.com>
2026-02-19 16:27:27 +0545

    Changes

--- core/urls/__init__.py
+++ core/urls/__init__.py
@@ -3,6 +3,7 @@ from core.views import callback_views
 
 urlpatterns = [
     path('callback/', callback_views.game_callback),
+    path('callback', callback_views.game_callback),
     path('public/', include('core.urls.public_urls')),
     path('powerhouse/', include('core.urls.powerhouse_urls')),
     path('super/', include('core.urls.super_urls')),
