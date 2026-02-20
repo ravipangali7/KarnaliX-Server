f314c3a401f030bf00a8baae967e7311e2396e94
Ravi Pangali <lilathapamagar453@gmail.com>
2026-02-20 23:42:08 +0545

    Chngaes

--- karnalix/asgi.py
+++ karnalix/asgi.py
@@ -5,13 +5,16 @@ Run with Daphne for WebSocket support: daphne -b 0.0.0.0 -p 8000 karnalix.asgi:a
 """
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "karnalix.settings")
+
+import django
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 from core.auth_middleware import TokenAuthMiddleware
 from core.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "karnalix.settings")
 
 django_asgi = get_asgi_application()
 
