be770d1e9eaba1bc7c70b0a546465068e88e28fd
Ravi Pangali <lilathapamagar453@gmail.com>
2026-02-23 18:02:59 +0545

    Changes

--- core/views/launch_views.py
+++ core/views/launch_views.py
@@ -52,7 +52,7 @@ def _launch_game_common(request):
             user_id=user_id,
             wallet_amount=wallet_amount,
             # game_uid=game_uid,
-            game_uid='08ced9dd788aed11ff3c7f387ae0f063',
+            game_uid='58817c1ce7f65322581e1be2aeadadee',
             domain_url=domain_url,
             allow_redirects=False,
         )
