ea92cf931babc2c303027d702599d6a5281ca7e7
Ravi Pangali <lilathapamagar453@gmail.com>
2026-02-23 15:52:29 +0545

    Changes

--- core/views/powerhouse/site_setting_views.py
+++ core/views/powerhouse/site_setting_views.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -16,6 +17,17 @@ def site_setting_get(request):
     return Response(SiteSettingSerializer(obj).data if obj else None)
 
 
+def _parse_promo_banners(value):
+    if value is None or value == '':
+        return []
+    if isinstance(value, list):
+        return value
+    try:
+        return json.loads(value)
+    except (TypeError, ValueError):
+        return []
+
+
 @api_view(['PUT', 'PATCH'])
 @permission_classes([IsAuthenticated])
 def site_setting_update(request):
@@ -23,7 +35,23 @@ def site_setting_update(request):
     if err:
         return err
     obj = SiteSetting.objects.first() or SiteSetting()
-    ser = SiteSettingSerializer(obj, data=request.data, partial=(request.method == 'PATCH'))
+    data = request.data
+
+    # Accept multipart/form-data for logo file upload
+    if request.FILES.get('logo'):
+        data = {
+            'name': request.data.get('name') or '',
+            'phones': [x for x in [request.data.get('phone1'), request.data.get('phone2')] if x],
+            'emails': [x for x in [request.data.get('email1')] if x],
+            'whatsapp_number': request.data.get('whatsapp_number') or '',
+            'hero_title': request.data.get('hero_title') or '',
+            'hero_subtitle': request.data.get('hero_subtitle') or '',
+            'footer_description': request.data.get('footer_description') or '',
+            'promo_banners': _parse_promo_banners(request.data.get('promo_banners')),
+            'logo': request.FILES.get('logo'),
+        }
+
+    ser = SiteSettingSerializer(obj, data=data, partial=(request.method == 'PATCH'))
     ser.is_valid(raise_exception=True)
     ser.save()
     return Response(ser.data)
