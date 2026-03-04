31d80a3747f0cbf3fecbb8df43767a675114631d
Ravi Pangali <lilathapamagar453@gmail.com>
2026-03-04 22:08:00 +0545

    CHanges

--- core/views/player/message_views.py
+++ core/views/player/message_views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, parser_classes
+from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
@@ -36,12 +37,20 @@ def message_unread_count(request):
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
+@parser_classes([FormParser, MultiPartParser])
 def message_create(request):
     err = require_role(request, [UserRole.PLAYER])
     if err: return err
-    receiver_id = request.data.get('receiver')
-    if receiver_id != request.user.parent_id: return Response({'detail': 'Invalid receiver.'}, status=status.HTTP_400_BAD_REQUEST)
-    ser = MessageCreateSerializer(data={**request.data, 'receiver': receiver_id})
+    try:
+        receiver_id = int(request.data.get('receiver'))
+    except (TypeError, ValueError):
+        return Response({'detail': 'Invalid receiver.'}, status=status.HTTP_400_BAD_REQUEST)
+    if receiver_id != request.user.parent_id:
+        return Response({'detail': 'Invalid receiver.'}, status=status.HTTP_400_BAD_REQUEST)
+    data = {k: v for k, v in request.data.items()}
+    data['receiver'] = receiver_id
+    data.update(request.FILES)
+    ser = MessageCreateSerializer(data=data)
     ser.is_valid(raise_exception=True)
     msg = ser.save(sender=request.user)
     data = MessageSerializer(msg).data
