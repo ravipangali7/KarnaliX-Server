1b0833dd5c4c705b15d03417d017d8d42a6945c3
targetdubai2026 <targetdubai2026@gmail.com>
2026-03-16 23:47:41 +0545

    Changes

--- karnalix/settings.py
+++ karnalix/settings.py
@@ -133,8 +133,8 @@ WSGI_APPLICATION = 'karnalix.wsgi.application'
 # SITE_DOMAIN = 'https://inr.luckyuser365.com'
 # SITE_DOMAIN = 'https://bdt.luckyuser365.com'
 # SITE_DOMAIN = 'https://myr.luckyuser365.com'
-SITE_DOMAIN = 'https://aed.luckyuser365.com'
-# SITE_DOMAIN = 'https://aud.luckyuser365.com'
+# SITE_DOMAIN = 'https://aed.luckyuser365.com'
+SITE_DOMAIN = 'https://aud.luckyuser365.com'
 
 DATABASES = {
     'default': {
@@ -143,8 +143,8 @@ DATABASES = {
         # 'NAME': 'inrluckyuser365',
         # 'NAME': 'bdtluckyuser365',
         # 'NAME': 'myrluckyuser365',
-        'NAME': 'aedluckyuser365',
-        # 'NAME': 'audluckyuser365',
+        # 'NAME': 'aedluckyuser365',
+        'NAME': 'audluckyuser365',
         'USER': 'root',
         'PASSWORD': '7a5a70064acdfd90',
         'HOST': '127.0.0.1',
