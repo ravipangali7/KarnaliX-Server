7b7907ccb57015430d1045101c6fa9e5204bbf43
targetdubai2026 <targetdubai2026@gmail.com>
2026-03-25 04:19:17 +0545

    Changes

--- karnalix/settings.py
+++ karnalix/settings.py
@@ -129,26 +129,33 @@ WSGI_APPLICATION = 'karnalix.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-# SITE_DOMAIN = 'https://luckyuser365.com'
 # SITE_DOMAIN = 'https://inr.luckyuser365.com'
 # SITE_DOMAIN = 'https://bdt.luckyuser365.com'
 # SITE_DOMAIN = 'https://myr.luckyuser365.com'
 # SITE_DOMAIN = 'https://aed.luckyuser365.com'
 # SITE_DOMAIN = 'https://aud.luckyuser365.com'
 # SITE_DOMAIN = 'https://bht.luckyuser365.com'
-SITE_DOMAIN = 'https://lucky365bht.com'
+
+# SITE_DOMAIN = 'https://luckyuser365.com'
+SITE_DOMAIN = 'https://lucky365ind.com'
+# SITE_DOMAIN = 'https://lucky365bhd.com'
+# SITE_DOMAIN = 'https://lucky365bht.com'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'luckyuser365',
         # 'NAME': 'inrluckyuser365',
         # 'NAME': 'bdtluckyuser365',
         # 'NAME': 'myrluckyuser365',
         # 'NAME': 'aedluckyuser365',
         # 'NAME': 'audluckyuser365',
         # 'NAME': 'bhtluckyuser365',
-        'NAME': 'lucky365bht',
+
+        # 'NAME': 'luckyuser365',
+        'NAME': 'lucky365ind',
+        # 'NAME': 'lucky365bhd',
+        # 'NAME': 'lucky365bht',
+
         'USER': 'root',
         'PASSWORD': '7a5a70064acdfd90',
         'HOST': '127.0.0.1',
